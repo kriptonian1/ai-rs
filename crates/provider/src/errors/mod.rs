111eaa49c338b50591859347b573a24a6b0f0912
Sawan Bhattacharya <shymal.sawan@gmail.com>
2025-12-24 21:32:47 +0530

    feat: add no such model error

--- crates/provider/src/errors/mod.rs
+++ crates/provider/src/errors/mod.rs
@@ -1,12 +1,13 @@
 pub mod api_call_error;
 mod empty_response_body_error;
 mod get_error_message;
-mod json_parse_error;
-mod sdk_errors;
 mod invalid_argument_error;
-mod type_validation_error;
 mod invalid_prompt_error;
 mod invalid_response_data_error;
+mod json_parse_error;
 mod load_api_key_error;
 mod load_setting_error;
 mod no_content_generated;
+mod no_such_model_error;
+mod sdk_errors;
+mod type_validation_error;
